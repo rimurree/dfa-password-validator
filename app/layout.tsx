dd2045c9de289aebb8ccc08c51b03b5577edd2a3
jersoncarin <jersoncarin25@gmail.com>
2025-04-17 21:25:03 +0800

    remove wtf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
